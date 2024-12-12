ca51d8f5ea47bfc2a3dec3a5716c057aba6b80dd
DavideRodeo <57793795+DavideRodeo@users.noreply.github.com>
2024-12-12 19:51:36 +0100

    Update noobai.sh

--- noobai.sh
+++ noobai.sh
@@ -19,14 +19,11 @@ PIP_PACKAGES=(
 )
 
 EXTENSIONS=(
-    "https://github.com/deforum-art/sd-webui-deforum"
-    "https://github.com/adieyal/sd-dynamic-prompts"
-    "https://github.com/ototadana/sd-face-editor"
     "https://github.com/AlUlkesh/stable-diffusion-webui-images-browser"
-    "https://github.com/hako-mikan/sd-webui-regional-prompter"
     "https://github.com/Coyote-A/ultimate-upscale-for-automatic1111"
     "https://github.com/hinablue/sd-forge-adetailer"
     "https://github.com/tritant/sd-webui-creaprompt"
+    "https://github.com/Haoming02/sd-forge-couple"
 )
 
 CHECKPOINT_MODELS=(
