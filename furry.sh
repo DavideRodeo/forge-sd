686e4efdbcca77f03155a63e7abaf68392de25dd
DavideRo <57793795+DavideRodeo@users.noreply.github.com>
2026-01-06 21:13:34 +0100

    Update furry.sh

--- furry.sh
+++ furry.sh
@@ -19,6 +19,7 @@ EXTENSIONS=(
     "https://github.com/Amadeus-AI/img2img-hires-fix.git"
     "https://github.com/MINENEMA/sd-webui-quickrecents.git"
     "https://github.com/altoiddealer/--sd-webui-ar-plusplus.git"
+    "https://github.com/Physton/sd-webui-prompt-all-in-one"
 )
 UNET_MODELS=()
 LORA_MODELS=()
